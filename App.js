b6dfa1d4df88053695c5e090f8e9fb31d0040ff9
WilsonLeung <wilsonleung0914@gmail.com>
2021-07-14 18:17:51 +0800

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, FlatList, Pressable, Alert } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const ListItem = (props) => {
+    return (
+        <View>
+            <Text onPress={() => Alert.alert(`The Capital of ${props.item.name} is ${props.item.capital}`)} style={[styles.text, {color: '#FAFAFA'}]}>{props.item.name},{props.item.capital}</Text>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        backgroundColor: "#FAFAFA",
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    // TODO: 2. Add custom styles
+    header: {
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+    // TODO: 5. Add List styles
+    listContainer: {
+        padding: 30,
+        backgroundColor: '#483D8B',
+        color: '#FAFAFA',
+    },
+    text: {
+        margin: 10,
+        fontSize: 20,
+    }
 });
+
+export default function App() {
+
+    {/* TODO: 3. Prepare Countries Data */}
+    // array data
+    const countriesArray = [{"name":"Afghanistan"},{"name":"Åland Islands"},{"name":"Albania"},{"name":"Algeria"}]
+    // api data
+    const [countriesData, setCountriesData] = useState([]);
+
+    // TODO: 6. Use Fetch API get countries data
+    function fetchCountriesData() {
+        fetch('https://restcountries.eu/rest/v2/region/asia?fields=name;capital;numericCode')
+            .then((response) => response.json())
+            .then((json) => setCountriesData(json))
+            .catch((error) => console.error(error));
+    }
+
+    useEffect(() => {
+        fetchCountriesData();
+    });
+
+    function _keyExtractor(item, index) {
+        return item.numericCode;
+    }
+
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <Text>Open up App.js to start working on your app!</Text>
+            {/* TODO: 1. Add Text Element */}
+            <Text style={styles.header}>Hello World!!!</Text>
+
+            {/* TODO: 4. Add List of Countries */}
+            {/* version 1. simple item, data from static array */}
+            <FlatList
+                data={countriesArray}
+                renderItem={({item}) => <Text style={styles.text}>{item.name}</Text>} />
+            {/* version 2. standalone item, data from api */}
+            <FlatList
+                data={countriesData}
+                contentContainerStyle={styles.listContainer}
+                renderItem={({item}) => <ListItem item={item} />}
+                keyExtractor={_keyExtractor} />
+        </View>
+    );
+}
